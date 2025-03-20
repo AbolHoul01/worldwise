56a6d80e1a02f2a564576f43d7e5cc568fa856e5
Abolfazl_Ramezani <abolhoul.dev@gmail.com>
2025-03-20 13:13:25 +0330

    Code splitting finish project

--- src/components/SpinnerFullPage/SpinnerFullPage.jsx
+++ src/components/SpinnerFullPage/SpinnerFullPage.jsx
@@ -1,10 +1,11 @@
-import Spinner from "./Spinner";
+
+import Spinner from "../Spinner/Spinner";
 import styles from "./SpinnerFullPage.module.css";
 
 function SpinnerFullPage() {
   return (
     <div className={styles.spinnerFullpage}>
-      <Spinner />
+     <Spinner />
     </div>
   );
 }
